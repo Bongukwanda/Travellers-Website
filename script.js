a9462ee7a2320fb2e990bb26944208a5abb8edeb
Bongukwanda <workkwanda@gmail.com>
2026-08-30 11:50:18 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const turinBtn = document.getElementById("turinBtn");
+const manchBtn = document.getElementById("manchBtn");
+const madridBtn = document.getElementById("madridBtn");
+
+function displayImage() {
+  let madridPic = document.querySelector('.cityImages');
+  madridPic.style.display = 'none';
+}
+
+madridBtn.addEventListener('click', () => displayImage);
